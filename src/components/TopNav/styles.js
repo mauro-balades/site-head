795282eeb705ae5fb13e5050429965d77c7b28ed
mauro-balades <mauro.balades@tutanota.com>
2022-04-22 21:28:53 +0200

    added github link for the top nav

--- src/components/TopNav/styles.js
+++ src/components/TopNav/styles.js
@@ -3,14 +3,27 @@ import styled from 'styled-components';
 export const NavigationWrapper = styled.nav`
     width: 100%;
     box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
-    padding: 20px;
+    padding: 20px 0;
     z-index: 9;
     background: ${({ theme }) => theme.leftNavBackgroundColor};
     position: absolute;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `;
 
 export const NavigationHeader = styled.h1`
     margin: 0;
     font-weight: 700;
     font-size: 20px;
+
+    margin-left: 40px;
+`;
+
+export const NavigationLinks = styled.div`
+    display: flex;
+    margin-right: 40px;
+
+    aling-items: center;
 `;
